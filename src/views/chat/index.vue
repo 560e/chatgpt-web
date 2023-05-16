ac16fe112c0c48abc40d1554612568db091db333
zhangxw560 <zhangxw560@sap560.com>
2023-05-16 17:55:06 +0800

    0.0.1

--- src/views/chat/index.vue
+++ src/views/chat/index.vue
@@ -481,7 +481,7 @@ onUnmounted(() => {
           <template v-if="!dataSources.length">
             <div class="flex items-center justify-center mt-4 text-center text-neutral-300">
               <SvgIcon icon="ri:bubble-chart-fill" class="mr-2 text-3xl" />
-              <span>Aha~</span>
+              <span>欢迎来到五六零网校</span>
             </div>
           </template>
           <template v-else>
